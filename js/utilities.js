11a115651f2f8bfc0c8b9386cb4fb0da1b3b41c2
unknown <acm3750@C2435-06.main.ad.rit.edu>
2015-02-11 14:23:41 -0500

    demo commit, everything will be deleted!

---
+++ js/utilities.js
@@ -0,0 +1,63 @@
+function getMouse(e)
+{			
+	var mouse = {};			
+	mouse.x = e.pageX - e.target.offsetLeft;
+	mouse.y = e.pageY - e.target.offsetTop;		
+	return mouse;
+}
+		
+function pointInsideCircle(x,y,I)
+{
+	var dx = x - I.x;
+	var dy = y - I.y;
+	return dx * dx + dy * dy <= I.radius * I.radius;
+}
+
+function circlesIntersect(c1, c2)
+{
+	var dx = c2.x - c1.x;
+	var dy = c2.y - c1.y;
+	var distance = Math.sqrt(dx * dx + dy * dy);
+	return distance < c1.radius + c2.radius;
+}
+
+function getRandomUnitVector()
+{
+	var x = getRandom(-1, 1);
+	var y = getRandom(-1, 1);
+	var length = Math.sqrt(x*x + y*y);
+	if (length == 0)
+	{
+		x = 1;
+		y = 0;
+	}
+	else
+	{
+		x /= length;
+		y /= length;
+	}
+	
+	return {x:x, y:y};
+}
+
+function getRandom(min, max)
+{
+	return Math.random() * (max-min) + min;
+}
+
+// returns a random color of alpha 1.0
+// http://paulirish.com/2009/random-hex-color-code-snippets/
+function getRandomColor(){
+	var red = Math.round(Math.random()*200+55);
+	var green = Math.round(Math.random()*200+55);
+	var blue=Math.round(Math.random()*200+55);
+	var color='rgb('+red+','+green+','+blue+')';
+	// OR	if you want to change alpha
+	// var color='rgba('+red+','+green+','+blue+',0.50)'; // 0.50
+	return color;
+}
+
+function clamp(val, min, max)
+{
+	return Math.max(min, Math.min(max, val));
+}
