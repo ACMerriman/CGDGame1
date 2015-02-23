9e5fb8a04d1ab0ed9342d47e1351145d257c8317
Andrew Merriman <ACMerriman@aol.com>
2015-02-23 12:25:15 -0500

    Added all of the things

--- js/bouller.js
+++ js/bouller.js
@@ -1,4 +1,7 @@
-"use strict"		
+"use strict"	
+
+var CIRCLE_SPEED = 200;
+	
 var Bouller = function(x, y, state) {
 
 	this.x = x;
@@ -7,86 +10,76 @@ var Bouller = function(x, y, state) {
 	this.state = state;
 	this.radius = 25;
 	this.color = "#FFF";
+}
 	
-	/*
-	function Bouller(x, y, state)
+var b = Bouller.prototype;
+
+b.update = function(dt, circles)
+{
+	for(var i = 0; i < circles.length; i++)
 	{
-		this.x = x;
-		this.y = y;
-		this.active = true;
-		this.state = state;
-		this.radius = 25;
+		if(this.collidesWithCircle(circles[i]))
+		{
+			this.state -= 1;
+			circles[i].active = false;
+		}
+	}
+	if(this.state <= 0)
+	{
+		this.explode(circles);
+	}
+	else if (this.state == 1)
+	{
+		this.color = "#F00";
+	}
+	else if (this.state == 2)
+	{
+		this.color = "#0F0";
+	}
+	else if (this.state == 3)
+	{
+		this.color = "#00F";
+	}
+	else if (this.state == 4)
+	{
+		this.color = "#0FF";
+	}
+	else if (this.state == 5)
+	{
+		this.color = "#F0F";
+	}
+	else if (this.state == 6)
+	{
+		this.color = "#FF0";
+	}
+	else if (this.state == 7)
+	{
 		this.color = "#FFF";
-	};
-	*/
-	
-	var b = Bouller.prototype;
-	
-	b.update = function(dt)
-	{
-		for(var i = 0; i < circles.length; i++)
-		{
-			if(this.collidesWithCircle(circles[i]))
-			{
-				this.state -= 1;
-				circles[i].active = false;
-			}
-		}
-		if(this.state == 0)
-		{
-			this.explode();
-		}
-		else if (this.state == 1)
-		{
-			this.color = "#F00";
-		}
-		else if (this.state == 2)
-		{
-			this.color = "#0F0";
-		}
-		else if (this.state == 3)
-		{
-			this.color = "#00F";
-		}
-		else if (this.state == 4)
-		{
-			this.color = "#0FF";
-		}
-		else if (this.state == 5)
-		{
-			this.color = "#F0F";
-		}
-		else if (this.state == 6)
-		{
-			this.color = "#FF0";
-		}
-		else if (this.state == 7)
-		{
-			this.color = "#FFF";
-		}
-	};
-	
-	b.draw = function(ctx)
-	{
-		ctx.save();
-		ctx.fillStyle = this.color;
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		ctx.closePath();
-		ctx.fill()
-	};
-	
-	b.collidesWithCircle = function(circle)
-	{
-		return circlesIntersect(this, circle);
-	};
-	
-	b.explode = function()
-	{
-		this.active = false;
-		circles.push(new circle(this.x, this.y,  10,   0));
-		circles.push(new circle(this.x, this.y, -10,   0));
-		circles.push(new circle(this.x, this.y,   0,  10));
-		circles.push(new circle(this.x, this.y,   0, -10));
-	};
-}();
+	}
+};
+
+b.draw = function(ctx)
+{
+	ctx.save();
+	ctx.fillStyle = this.color;
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+	ctx.closePath();
+	ctx.fill()
+};
+
+b.collidesWithCircle = function(circle)
+{
+	return circlesIntersect(this, circle);
+};
+
+b.explode = function(circles)
+{
+	this.active = false;
+	circles.push(new Circle(this.x, this.y,  CIRCLE_SPEED, 			   0));
+	circles.push(new Circle(this.x, this.y, -CIRCLE_SPEED,		   	   0));
+	circles.push(new Circle(this.x, this.y, 			0,  CIRCLE_SPEED));
+	circles.push(new Circle(this.x, this.y, 			0, -CIRCLE_SPEED));
+	console.log("boom");
+};
+//}();
--- js/circle.js
+++ js/circle.js
@@ -1,32 +1,37 @@
-		
-var Circle = function() {
-	function Circle(x, y, xSpeed, ySpeed)
+
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 480;
+
+function Circle(x, y, xSpeed, ySpeed)
+{
+	this.x = x;
+	this.y = y;
+	this.active = true;
+	this.xSpeed = xSpeed;
+	this.ySpeed = ySpeed;
+	this.radius = 0;
+	this.drawRadius = 10;
+	this.color = "#FFF";
+};
+
+var c = Circle.prototype;
+
+c.update = function(dt)
+{
+	this.x += this.xSpeed * dt;
+	this.y += this.ySpeed * dt;
+	if(this.x < 0 || this.y < 0 || this.x > CANVAS_WIDTH ||this.y > CANVAS_HEIGHT)
 	{
-		this.x = x;
-		this.y = y;
-		this.active = true;
-		this.xSpeed = xSpeed;
-		this.ySpeed = ySpeed;
-		this.radius = 0;
-		this.drawRadius = 10;
-		this.color = "#FFF";
-	};
-	
-	var c = Circle.prototype;
-	
-	c.update = function(dt)
-	{
-		this.x += this.xSpeed * dt;
-		this.y += this.ySpeed * dt;
-	};
-	
-	c.draw = function(ctx)
-	{
-		ctx.save();
-		ctx.fillStyle = this.color;
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.drawRadius, 0, Math.PI * 2, false);
-		ctx.closePath();
-		ctx.fill()
-	};
-}();
+		this.active = false;
+	}
+};
+
+c.draw = function(ctx)
+{
+	ctx.save();
+	ctx.fillStyle = this.color;
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, this.drawRadius, 0, Math.PI * 2, false);
+	ctx.closePath();
+	ctx.fill()
+};
